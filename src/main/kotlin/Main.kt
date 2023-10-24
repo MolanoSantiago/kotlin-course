b51b67eb73a93ee145e2f7a4cfa1c7e450cc1b55
MolanoAlternova <santiago.molano@alternova.com>
2023-10-23 21:43:58 -0500

    Variables types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,15 +2,38 @@ const val NAME = "Santiago Molano";
 
 fun main(args: Array<String>) {
     // immutable
+    // String
     val profession: String = "Backend developer";
 
     // mutable
+    // Integer
     var money: Int = 80000;
 
-    println("Hola $NAME! su saldo actual: $money COP");
+    // Interpolation or templates
+    println("Hola $NAME! su saldo actual: $ $money COP");
     println("");
     money = 45000;
     println("Qué desea hacer hoy como $profession?");
     println("");
-    println("Se ha gastado 35000 COP, ahora tiene $money");
+    println("Se ha gastado $ 35000 COP, ahora tiene $ $money");
+
+    // Boolean
+    val boolean: Boolean = true;
+    println("");
+    println("Disponible avance en pesos: $boolean");
+
+    // Long
+    val long: Long = 123456789123456789;
+    println("");
+    println("Cantidad avance en pesos: $ $long");
+
+    // Double
+    val double: Double = 12500.87;
+    println("");
+    println("Debe exactamente en la TC: $ $double");
+
+    // Float
+    val float: Float = 1.1f;
+    println("");
+    println("Cashback de este mes: $ $float");
 }
